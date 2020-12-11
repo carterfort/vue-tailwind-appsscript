8b5c29e4b31dc254bb2bc1b00984f8670d94ad31
Carter Fort <carter.fort@gmail.com>
2020-12-11 09:11:23 -0500

    Implement a Notification component for display purposes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-    <div class="container pt-3">
-        This is the main application container.
-        <br/>
-        Are we still good here?
+    <div class="container pt-3 mx-auto">
+        <notification 
+                title="ChitChat" 
+                message="Tailwind has started working!"
+                class=""
+            />
     </div>
 </template>
 
 <script>
+
+import Notification from './components/Notification'
+
 export default {
     props : [],
+    components : {
+        Notification
+    },
     data(){
         return {
 
---
+++ client/src/components/Notification.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+  <div>
+    <div class="text-xl font-medium text-black">{{ title }}</div>
+    <p class="text-gray-500">{{ message }}</p>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    props : ['title', 'message'],
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+
+    },
+    methods : {
+
+    }
+}
+</script>
+
+<style>
+</style>
