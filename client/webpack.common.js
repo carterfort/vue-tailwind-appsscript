98b3b391d28c5add88dc0b6961875d852386c5f6
Carter Fort <carter.fort@gmail.com>
2020-12-10 22:14:20 -0500

    Trying it this way :shrug:

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,23 +1,19 @@
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     entry: {
-        app: './client/src/main.js',
-        style : './client/src/css/app.css'
+        app: './client/src/main.js'
+    },
+    stats: {
+        performance: false
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        { loader: 'css-loader', options: { importLoaders: 1 } },
-                        'postcss-loader',
-                    ],
-                }),
+                use: ["css-loader", "postcss-loader"]
             },
             {
                 test: /\.vue$/,
@@ -32,9 +28,6 @@ module.exports = {
         extensions: ['*', '.js', '.vue', '.json']
     },
     plugins: [
-        new ExtractTextPlugin('styles.css', {
-            disable: process.env.NODE_ENV === 'development',
-        }),
         new VueLoaderPlugin()
     ]
 
