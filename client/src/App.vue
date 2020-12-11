5b0989b4e444b1bbe9f5dda0a3255bf27ec34095
Carter Fort <carter.fort@gmail.com>
2020-12-11 09:45:13 -0500

    Adding the ability to add more messages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="container pt-3 mx-auto">
+        <div class="text-right w-1/2 mx-auto mb-9">
+            <input class="w-full mt-1 px-2 py-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" v-model="newTitle" placeholder="Title">
+            <textarea rows="3" class="w-full px-2 py-1 resize-none shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" v-model="newMessage" placeholder="Message"></textarea>
+            <button 
+                @click="postNewMessage"
+                class="mt-3 ml-auto bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >Post
+            </button>
+        </div>
         <notification 
-                title="ChitChat" 
-                message="Tailwind has started working!"
-                class=""
+                :key="index"
+                v-for="(msg, index) in messages"
+                :message="msg.message"
+                :title="msg.title"
+                class="mb-2"
             />
     </div>
 </template>
@@ -19,14 +30,26 @@ export default {
     },
     data(){
         return {
-
+            messages : [
+                {
+                    "title" : "ChitChat",
+                    "message" : "Tailwind has started working!"
+                }
+            ],
+            newTitle : '',
+            newMessage : ''
         }
     },
     mounted(){
 
     },
     methods : {
-
+        postNewMessage(){
+            let msg = {title : this.newTitle, message: this.newMessage}
+            this.newTitle = '';
+            this.newMessage = '';
+            this.messages.push(msg);
+        }
     }
 }
 </script>
