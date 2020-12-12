85b2882396b2149a4f9c76e666f9b2164dfdc828
Carter Fort <carter.fort@gmail.com>
2020-12-12 12:38:56 -0500

    Adding a globally accessible network activity indicator

---
+++ client/src/components/NetworkActivityIndicator.vue
@@ -0,0 +1,41 @@
+<template>
+    <div :style="style" class="bg-blue-800 text-white flex items-center justify-self-center transition-all ease-in-out">
+        <div class="mx-auto flex items-center">
+        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        {{this.message}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props : [],
+    data(){
+        return {
+            message : ''
+        }
+    },
+    computed : {
+        style() {
+            return {
+                height: this.message ? '80px' : '0px'
+            }
+        }
+    },
+    mounted(){
+        window.Events.$on('displayMessage', (message) => {
+            this.message = message;
+        })
+
+        window.Events.$on('dismissMessage', () => {
+            this.message = '';
+        })
+    },
+    methods : {
+
+    }
+}
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import App from './App'
 import AppScript from './services/BackEndPlugin'
+import NetworkActivityIndicator from './components/NetworkActivityIndicator'
 
 Vue.use(AppScript)
 
 
 import './css/app.css'
+window.Events = new Vue({});
+
+Vue.component('network-activity-indicator', NetworkActivityIndicator);
 
 new Vue({
     el: '#app',
