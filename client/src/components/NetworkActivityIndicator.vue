fa64fdd8a8e1d7ac20ffaf7da8ab922f67253f9b
Carter Fort <carter.fort@gmail.com>
2020-12-12 12:44:25 -0500

    Hide the spinner when it's not doing anything

--- client/src/components/NetworkActivityIndicator.vue
+++ client/src/components/NetworkActivityIndicator.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :style="style" class="bg-blue-800 text-white flex items-center justify-self-center transition-all ease-in-out">
+    <div :style="style" class="bg-blue-800 text-white flex items-center justify-self-center transition-all ease-in-out overflow-hidden">
         <div class="mx-auto flex items-center">
         <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
