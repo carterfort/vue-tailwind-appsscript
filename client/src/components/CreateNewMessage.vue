52c6f8b613ff83e1a1a3a5aff694b1546aeb29d2
Carter Fort <carter.fort@gmail.com>
2020-12-12 19:08:28 -0500

    set button class

--- client/src/components/CreateNewMessage.vue
+++ client/src/components/CreateNewMessage.vue
@@ -4,7 +4,7 @@
         <textarea rows="3" class="w-full px-2 py-1 resize-none shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" v-model="newMessage" placeholder="Message"></textarea>
         <button 
             @click="postNewMessage"
-            class="mt-3 ml-auto bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            class="mt-3 ml-auto btn btn-primary"
         >Post
         </button>
     </div>
