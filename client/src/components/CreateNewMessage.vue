0bfbf31b2c2ebc1ab426ec14e3d06f2a2ef1e3a3
Carter Fort <carter.fort@gmail.com>
2020-12-12 21:32:35 -0500

    Add form styles

--- client/src/components/CreateNewMessage.vue
+++ client/src/components/CreateNewMessage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <input class="w-full mt-1 px-2 py-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" v-model="newTitle" placeholder="Title">
-        <textarea rows="3" class="w-full px-2 py-1 resize-none shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" v-model="newMessage" placeholder="Message"></textarea>
+        <input class="form-control" v-model="newTitle" placeholder="Title">
+        <textarea rows="3" class="form-control" v-model="newMessage" placeholder="Message"></textarea>
         <button 
             @click="postNewMessage"
             class="mt-3 ml-auto btn btn-primary"
